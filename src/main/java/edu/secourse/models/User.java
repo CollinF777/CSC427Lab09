77d1667a313ca5abe56b5b6db08f70e6309ff917
Collin F <thedrunkninja98@gmail.com>
2025-10-29 15:37:50 -0400

    Added user and its children

---
+++ src/main/java/edu/secourse/models/User.java
@@ -0,0 +1,49 @@
+package edu.secourse.models;
+
+public abstract class User {
+    private int accNum;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+
+    public User(int accNum, String username, String password, String name, String email) {
+        this.accNum = accNum;
+        this.password = password;
+        this.username = username;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getAccNum() {
+        return accNum;
+    }
+
+    public void setAccNum(int accNum) {
+        this.accNum = accNum;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
