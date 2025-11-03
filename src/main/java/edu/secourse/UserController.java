4714d9065cf1865877421f6042149dad073faf5d
Corey Suhr <36052723+CaptainTuba52@users.noreply.github.com>
2025-11-03 14:36:58 -0500

    Update UserController.java

--- src/main/java/edu/secourse/UserController.java
+++ src/main/java/edu/secourse/UserController.java
@@ -15,17 +15,15 @@ public class UserController {
     UserService uService = new UserService();
 
     public User createUser(String username, String password, String name, String emailAddress, String role){
-        User newUser = uService.createUser(username, password, name, emailAddress, role);
-        return newUser;
+        return uService.createUser(username, password, name, emailAddress, role);
     }
     public void updateUser(User user){
         uService.updateUser(user);
     }
     public void removeUser(int id){
-        User user = uService.getUser(id);
-        boolean x = uService.deleteUser(user);
+        boolean x = uService.deleteUser(id);
 
-        if(x == false){
+        if(!x){
             throw new RuntimeException("Cannot delete a user that does not exist.");
         }
     }
